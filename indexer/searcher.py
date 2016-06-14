11b1b7560df1694be3f3b5cfaf8a193d23873598
Pratyush Singh <singh.pratyush96@gmail.com>
2016-06-14 16:12:28 +0530

    Moved to ..

---
+++ indexer/searcher.py
@@ -0,0 +1,34 @@
+import xapian
+
+from crawler.db_operations import get_song_by_id
+
+
+def search(parameter, page=0, number=10):
+    db = xapian.Database('db')
+
+    qp = xapian.QueryParser()
+    qp.add_prefix('id', 'Q')
+    qp.add_prefix('song_name', 'XS')
+    qp.add_prefix('movie', 'XM')
+    qp.add_prefix('lyrics', 'XL')
+    qp.add_prefix('singers', 'XI')
+    qp.add_prefix('directors', 'XD')
+    qp.add_prefix('lyricists', 'XY')
+
+    qry = qp.parse_query(parameter)
+
+    enquire = xapian.Enquire(db)
+    enquire.set_query(qry)
+
+    results = enquire.get_mset(page * number, number)
+
+    result_set = []
+
+    for result in results:
+        result_set.append(
+            get_song_by_id(
+                str(result.document.get_data())[2:-1]
+            )
+        )
+
+    return result_set
